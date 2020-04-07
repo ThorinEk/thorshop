f30f630c5b5252aeadb107d3a83d7382631b14a5
BuildTools <unconfigured@null.spigotmc.org>
2020-04-07 23:06:13 +0200

    ThorShop första commit

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="sv">
+<title>Thorshop</title>
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="Thorshop - prisvärda läkemedelsprodukter med hög kvalitet">
+  <meta name="keywords" content="Nordatlas, svensk minecraft, test">
+  <meta name="author" content="Thorshop">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--Reloader - Ta bort innan publicering
+  <meta http-equiv="refresh" content="8">-->
+  <!--CSS Stilmall-->
+  <link rel="stylesheet" type="text/css" href='style/style.css'>
+  <link rel="stylesheet" type="text/css" href='style/mobile.css'>
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div class="MainMenu">
+        <span class="logoContainer">
+            <a>
+                <img class="sitelogo" src="https://upload.wikimedia.org/wikipedia/commons/7/74/ICA-logotyp.png">
+            </a>
+        </span>
+        <div class="wrapper">
+            <ul>
+                <li><a href="#chark">Chark</a></li>
+                <li><a href="#chark">Frukt</a></li>
+                <li><a href="#chark">Bageri</a></li>
+            </ul>
+        </div>
+    </div>
+<div class="wrapper main">   
+    <h1 id="chark">Chark</h1>
+    <p>Saftig biff, fågel eller fina fisken. Här kan du handla allt för din gryta, gratäng och grillat.</p>
+
+    <div class="grid-container">
+        <div class="grid-item">
+            <div class="grid-img-container">
+                <img src="https://i.imgur.com/xasAyKP.png">
+            </div>
+            <h3>Raw Porkchop</h3>
+            <div class="number">
+                <span class="minus">-</span>
+                <input type="text" value="1"/>
+                <span class="plus">+</span>
+            </div>
+        </div>
+        <div class="grid-item">
+            <div class="grid-img-container">
+                <img src="https://i.imgur.com/xasAyKP.png">
+            </div>
+            <h3>Raw Porkchop</h3>
+            <div class="number">
+                <span class="minus">-</span>
+                <input type="text" value="1"/>
+                <span class="plus">+</span>
+            </div>
+        </div>
+        <div class="grid-item">
+            <div class="grid-img-container">
+                <img src="https://i.imgur.com/xasAyKP.png">
+            </div>
+            <h3>Raw Porkchop</h3>
+            <div class="number">
+                <span class="minus">-</span>
+                <input type="text" value="1"/>
+                <span class="plus">+</span>
+            </div>
+        </div>
+        <div class="grid-item">
+            <div class="grid-img-container">
+                <img src="https://i.imgur.com/xasAyKP.png">
+            </div>
+            <h3>Raw Porkchop</h3>
+            <div class="number">
+                <span class="minus">-</span>
+                <input type="text" value="1"/>
+                <span class="plus">+</span>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="script/script.js"></script>
+</body>
+</html>
