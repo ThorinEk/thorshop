ec9ec61adafd12f381f5a7fe64b3e703f5732ff6
BuildTools <unconfigured@null.spigotmc.org>
2020-04-12 17:53:45 +0200

    Fixat footer och fruktavdelning

--- index.php
+++ index.php
@@ -17,16 +17,16 @@
 </head>
 <body>
     <div class="MainMenu">
+        <div class="wrapper">
         <span class="logoContainer">
             <a>
-                <img class="sitelogo" src="https://upload.wikimedia.org/wikipedia/commons/7/74/ICA-logotyp.png">
+                <img class="sitelogo" src="./img/ica.jpg">
             </a>
         </span>
-        <div class="wrapper">
             <ul>
                 <li><a href="#chark">Chark</a></li>
-                <li><a href="#chark">Frukt</a></li>
-                <li><a href="#chark">Bageri</a></li>
+                <li><a href="#frukt">Frukt</a></li>
+                <li><a href="#bageri">Bageri</a></li>
             </ul>
         </div>
     </div>
@@ -47,25 +47,20 @@
             </div>
         </div>
     </div>
-<div class="wrapper main">   
-    <h1 id="chark">Chark</h1>
+<div class="wrapper main">
+    <a class="anchor" id="chark"></a>
+    <h1>Chark</h1>
     <p>Saftig biff, fågel eller fina fisken. Här kan du handla allt för din gryta, gratäng och grillat.</p>   
     <div class="grid-container">
     <?php
     $filename = 'database/chark.csv';
-    // The nested array to hold all the arrays
     $the_big_array = []; 
-    // Open the file for reading
     if (($h = fopen("{$filename}", "r")) !== FALSE) 
     {
-    // Each line in the file is converted into an individual array that we call $data
-    // The items of the array are comma separated
     while (($data = fgetcsv($h, 1000, ";")) !== FALSE) 
     {
-        // Each individual array is being pushed into the nested array
         $the_big_array[] = $data;		
     }
-    // Close the file
     fclose($h);
     }
     for ($i = 2; $i < count($the_big_array); $i++){ ?>
@@ -87,6 +82,53 @@
             </div>
         </div>
     <?php } ?>
+    </div>
+
+    <br><br>
+    <hr>
+    <a class="anchor" id="frukt"></a>
+    <h1>Frukt</h1>
+    <p>Här hittar du alltid färsk frukt att fylla upp fruktkorgen med. Goda äpplen, apelsiner och bananer blir en god fruktsallad eller varför inte en uppfriskande smoothie?</p>
+    
+    <div class="grid-container">
+    <?php
+    $filename = 'database/bageri.csv';
+    $the_big_array = []; 
+    if (($h = fopen("{$filename}", "r")) !== FALSE) 
+    {
+    while (($data = fgetcsv($h, 1000, ";")) !== FALSE) 
+    {
+        $the_big_array[] = $data;		
+    }
+    fclose($h);
+    }
+    for ($i = 2; $i < count($the_big_array); $i++){ ?>
+        <div class="grid-item">
+            <div class="grid-img-container">
+                <img src="<?php echo $the_big_array[$i][3]; ?>">
+            </div>
+            <h3><?php echo $the_big_array[$i][1]; ?></h3>
+            <div class="grid-text-container">
+                <p class="price"><?php echo $the_big_array[$i][2]; ?></p><p class="unit">kr</p>
+            </div>
+            <div class="number">
+                <span class="minus">-</span>
+                <input class="quantity" type="text" value="0"/>
+                <span class="plus">+</span>
+            </div>
+            <div class="btn_container">
+                <button class="add_to_cart">Lägg i kundvagnen</button>
+            </div>
+        </div>
+    <?php } ?>
+    </div>
+
+    <div class="pageBottom">
+        <div class="bottom-container">
+            <img class="sitelogo" src="./img/ica.jpg">
+            <p><strong>ICA Centrallager</strong><br>Torsvik<br>Logpoint South Sweden</p>
+            <p><strong>Information</strong><br><a href="terms">Köpvillkor</a><br><a href="about">Så går det till</a></p>
+        </div>
     </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
